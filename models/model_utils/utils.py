8f32efb2c9f576523508bdfde8459bda0d292445
Jorgedavyd <jorged.encyso@gmail.com>
2023-07-06 08:07:13 -0400

    structure

---
+++ models/model_utils/utils.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+#utils
+
+class TrainDetectionPhase(nn.Module):
+    def training_step(self, batch):
+        inputs, targets = batch
+        # Reshape target tensor to match the input size
+        target_tensor = targets.unsqueeze(1)  # Add a new dimension
+        target_tensor = target_tensor.expand(-1, 1)  # Duplicate values across second dimension
+        out = self(inputs)                  # Generar predicciones
+        loss = binary_cross_entropy(out, target_tensor) # Calcular el costo
+        return loss
+
+    def validation_step(self, batch):
+        inputs, targets = batch
+        target_tensor = targets.unsqueeze(1)  # Add a new dimension
+        target_tensor = target_tensor.expand(-1, 1)  # Duplicate values across second dimension
+        out = self(inputs)                    # Generar predicciones
+        loss = binary_cross_entropy(out, target_tensor)   # Calcular el costo
+        acc = accuracy(out, targets) #Calcular la precisión
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Sacar el valor expectado de todo el conjunto de costos
+        batch_acc = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_acc).mean()   # Sacar el valor expectado de todo el conjunto de precisión
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result): # Seguimiento del entrenamiento
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
