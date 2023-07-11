694d98ecb49b12bed4cfacf9c2ee36e2519fb6a8
Jorgedavyd <jorged.encyso@gmail.com>
2023-07-11 16:56:46 -0400

    face_api_4

---
+++ training/face_models/default/face.py
@@ -0,0 +1,29 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+human_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    humans = human_cascade.detectMultiScale(gray, 1.9, 1)
+    
+    # Display the resulting frame
+    for (x,y,w,h) in humans:
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+         
+         
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
--- training/face_models/face_detection.py
+++ training/face_models/face_detection.py
@@ -2,17 +2,9 @@
 from ultralytics import YOLO
 import os
 
-def transport():
-    folder = os.listdir('/runs')[-1]
-    path = os.path.join('/runs', folder, 'weights', 'best.pt')        
-    models_path = os.abspath(os.path.join('/SADI-AI', 'models'))
-    os.rename(path, models_path)
-
 def train():
     # Load a model
     model = YOLO('yolov8n.yaml')  # build a new model from YAML
 
     # Train the model
     results = model.train(data='face_detection.yaml', epochs=100, imgsz=640)
-
-    transport()
